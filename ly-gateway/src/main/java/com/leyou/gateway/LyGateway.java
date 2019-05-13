d8b60f4957f31562a8c3f1572471e98532f8d2c0
CHEN <2878466@qq.com>
2019-05-13 14:10:30 +0800

    init

---
+++ ly-gateway/src/main/java/com/leyou/gateway/LyGateway.java
@@ -0,0 +1,15 @@
+package com.leyou.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+//@EnableEurekaClient
+@EnableZuulProxy
+@SpringCloudApplication
+public class LyGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(LyGateway.class);
+    }
+}
