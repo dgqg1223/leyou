d303b1f774e53c335a9af1c85c69652a4d2d06df
CHEN <2878466@qq.com>
2019-05-19 20:13:35 +0800

    add

--- ly-common/src/main/java/com/leyou/common/emums/ExceptionEnum.java
+++ ly-common/src/main/java/com/leyou/common/emums/ExceptionEnum.java
@@ -9,10 +9,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public enum ExceptionEnum {
 
-    PRICE_CANNOT_BE_NOLL(400,"价格不能为空")
+    PRICE_CANNOT_BE_NOLL(400,"价格不能为空"),
+    CATEGORY_NOT_FOND(404,"商品分类没有查到"),
     ;
 //    private static final ExceptionEnum ff = new ExceptionEnum(1,"");
     private int code;
     private String msg;
-
 }
--- ly-item/ly-item-service/src/main/java/com/leyou/LyitemApplication.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyitemApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
 
 //@EnableAutoConfiguration
 //@ComponentScan(basePackages = "com.leyou.item.web")
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan("com.leyou.item.mapper")
 public class LyitemApplication {
     public static void main(String[] args) {
 
